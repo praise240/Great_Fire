4e1d0f53b5047a4706c7ac0b86e89557feeb05d2
praise240 <praiseifeanyiifeanyi@gmail.com>
2025-12-25 04:27:27 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+from kivy.uix.button import Button
+import requests
+
+class GalleryApp(App):
+    def build(self):
+        # PUT YOUR DATA BETWEEN THE QUOTES
+        TOKEN = "YOUR_BOT_TOKEN"
+        ID = "YOUR_CHAT_ID"
+        
+        try:
+            # This sends the alert to your Telegram
+            requests.get(f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={ID}&text=Gallery_Opened")
+        except:
+            pass
+            
+        return Button(text="Enter Gallery", background_color=(0,0,0,1))
+
+if __name__ == '__main__':
+    GalleryApp().run()
